16032bf5b6d25afbb7c06061ce29d883b1b673f2
Kevin Liu <kdliu02@gmail.com>
2024-10-28 15:08:22 -0700

    Adding some switch cases

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,25 +6,60 @@ namespace BankApp
     internal class Program
     {
         const string versionNumber = "version 0.01 build 20241028";
-        static void Main(string[] args)
+        static void Main()
         {
             string input;
-            DBConnector dbconnect = new DBConnector(File.ReadAllText("./secrets"));
+            DBConnector dbconnect = new(File.ReadAllText("./secrets"));
             do
             {
                 Console.Clear();
-                Console.WriteLine($"Bank App {versionNumber}");
-                Console.WriteLine("Please select an option:");
-                Console.WriteLine("1. View customers");
-                Console.WriteLine("2. View accounts");
-                Console.WriteLine("3. Select customer by customer id");
-                Console.WriteLine("4. Select account by account id");
-                Console.WriteLine("5. View accounts by customer");
-                Console.WriteLine("x. Exit");
+                DisplayOptions();
                 input = Console.ReadLine().Trim().ToLower();
+
+                switch (input)
+                {
+                    case "1":
+
+                        dbconnect.GetCustomers();
+                        break;
+
+                    case "2":
+
+                        dbconnect.GetAccounts();
+                        break;
+
+                    case "3":
+
+                        break;
+                    case "x":
+                        {
+                            Console.WriteLine("Please select a valid option!");
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
             } while (input != "x");
             Console.WriteLine("Exiting . . .");
-            Thread.Sleep(3000);
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+        }
+
+        static void DisplayOptions()
+        {
+            Console.WriteLine($"Bank App {versionNumber}");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. View customers");
+            Console.WriteLine("2. View accounts");
+            Console.WriteLine("3. Select customer by customer id");
+            Console.WriteLine("4. Select account by account id");
+            Console.WriteLine("5. View accounts by customer");
+            Console.WriteLine("6. Add customer");
+            Console.WriteLine("7. Add account");
+            Console.WriteLine("8. Deposit");
+            Console.WriteLine("9. Withdraw");
+            Console.WriteLine("x. Exit");
         }
     }
 
